920a90708b4ee91af518422691ada761cd3e6321
kzhoru <adrianputraperkasa16@gmail.com>
2025-03-24 00:24:03 +0700

    implementasi design by contract

--- tpmodul6_103022300041/Program.cs
+++ tpmodul6_103022300041/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class SayaTubeVideo
 {
@@ -8,15 +9,29 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(!string.IsNullOrEmpty(title) && title.Length <= 100, "Judul video tidak boleh null atau lebih dari 100 karakter.");
+
         Random random = new Random();
-        this.id = random.Next(10000, 100000);
+        this.id = random.Next(10000, 100000); // 5 digit random number
         this.title = title;
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int increment)
     {
-        playCount += increment;
+        Debug.Assert(increment > 0 && increment <= 10000000, "Increment play count harus antara 1 hingga 10.000.000.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += increment;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow terjadi! Play count mencapai batas maksimum integer.");
+        }
     }
 
     public void PrintVideoDetails()
@@ -31,8 +46,21 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Adrian Putra Perkasa]");
-        video.IncreasePlayCount(5);
-        video.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Adrian Putra Perkasa]");
+            for (int i = 0; i < 100000; i++) // Loop besar untuk mempercepat overflow
+            {
+                video.IncreasePlayCount(1000000); // Tes overflow
+            }
+            video.PrintVideoDetails();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Terjadi error: " + ex.Message);
+        }
+
+        Console.WriteLine("Program selesai.");
+        Console.ReadLine();
     }
 }
